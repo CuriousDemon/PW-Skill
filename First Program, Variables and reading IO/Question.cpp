3fd3d740bd2bdc9f3a768d58ef52790129253ce8
Himangshu <himangs503@gmail.com>
2023-12-24 17:28:36 +0530

    Solve question number one

---
+++ First Program, Variables and reading IO/Question.cpp
@@ -0,0 +1,19 @@
+// Take 2 integer values in two variables x and y and print their product.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    x = 2; y = 4;
+    cout << "x*y = "<<x*y<<endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
